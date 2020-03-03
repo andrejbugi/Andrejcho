34748b19f3563dffb226af5fa8a4d7210941c17b
andrejbugi <andrejbugi96@gmail.com>
2020-03-03 23:31:20 +0100

    new methods and tests

--- lib/car.rb
+++ lib/car.rb
@@ -11,6 +11,23 @@ module Car
       @year = year
       @car_speed = 0.0
     end
-    
+
+    def speed_up(increase)
+      @increase = increase
+      @car_speed = @car_speed + @increase
+    end
+
+    def slow_down(decrease)
+      @decrease = decrease
+      @car_speed = @car_speed - @decrease
+    end
+
+    def shut_down
+      if @car_speed == 0
+        puts 'Engine off.'
+      else
+        puts 'Cannot shutdown while driving!'
+      end
+    end
   end
 end
--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe Car::MyCar do
         volvo = Car::MyCar.new('volvo', 2018, 'yellow')
         expect(volvo.car_speed).to eq(0)
     end
+
+    it "is able to speed up" do
+        merc = Car::MyCar.new('mercedes', 2015, 'black')
+        merc.speed_up(45)
+        expect(merc.car_speed).to be_within(44).of(46)
+    end
+
+    it "is braking great" do
+        merc = Car::MyCar.new('mercedes', 2015, 'black')
+        merc.slow_down(40)
+        expect(merc.car_speed).to be == -40
+    end
+    
+    it "turn off working correctly" do
+        merc = Car::MyCar.new('mercedes', 2015, 'black')
+        expect(merc.shut_down).to be_nil
+        merc.speed_up(40)
+        merc.slow_down(30)
+        expect(merc.shut_down).to be_nil
+    end
 end
